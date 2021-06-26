9279a69115e22915dd162bb5f64bc606c820b708
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-06-26 10:12:47 -0700

    Update

---
+++ NucampFolder/2-Bootstrap/nucampsite/js/scripts.js
@@ -0,0 +1,29 @@
+//Carousel
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel("pause");
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+        $(".carousel").carousel("cycle");
+        $("#carouselButton").children("i").removeClass("fa-play");
+        $("#carouselButton").children("i").addClass("fa-pause"); 
+    }
+    });
+});
+
+// Reserve Button
+$(function () {
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show');
+    });
+});
+
+// Login Button
+$(function () {
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
+    });
+});
