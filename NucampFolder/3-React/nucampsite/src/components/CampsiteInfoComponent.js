0223d8b607792ac00428037a59114ef523e8c58e
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-07-24 12:30:52 -0700

    Update CampsiteInfoComponent.js

--- NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -23,8 +23,7 @@ export default class CampsiteInfo extends Component {
             return(
                 <div className='col-md-5 m-1'>
                     <h4> Comments </h4>
-                    { comments.map(
-                        comment => 
+                    { comments.map( comment => 
                         <div> 
                             <p> {comment.text} </p>
                             <p> -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
