9fce96ee00028523d167bb168aa8541f7a4cc514
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-07-26 16:02:37 -0700

    asdsadasfdgfl;ghdfkjklgvserjgvlskg;bsvlgdfs

--- NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -36,17 +36,17 @@ export default class CampsiteInfo extends Component {
         return <div/>
     };
 
-    render(){
-        
-        if(this.props.campsite){
-            return(
-                <div className = "row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
-        
-        return <div/>;
+        return <div />;
     }
 }
