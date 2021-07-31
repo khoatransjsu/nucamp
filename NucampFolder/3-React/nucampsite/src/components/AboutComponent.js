c2a4d0ce7e930bf0e1f04e3d4efb37e32cf15468
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-07-31 12:51:05 -0700

    update

--- NucampFolder/3-React/nucampsite/src/components/AboutComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/AboutComponent.js
@@ -2,12 +2,29 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function RenderPartner({partner}) {
+    if(partner){
+        return (
+            <>
+                <Media object src={partner.image} alt = {partner.name} width ="150"/>
+                <Media body className="ml-5 mb-4">
+                    <Media heading>
+                        {partner.name}
+                    </Media>
+                    {partner.description}
+                </Media>
+            </>
+        )
+    }
+    return <div/>;
+}
 
 function About(props) {
-
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                 <RenderPartner partner = {partner}/>
+            </Media>
         );
     });
 
--- NucampFolder/3-React/nucampsite/src/components/MainComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
@@ -48,6 +49,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
