eb3c67c1238d6a75a961587038f51593d2cb68cd
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-08-07 14:30:33 -0700

    Update CampsiteInfoComponent.js

--- NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import {Button, Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import React, { Component } from 'react';
 import {Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
 
@@ -111,7 +112,24 @@ class CommentForm extends Component {
                             </div>
                             <div className="form-group">
                                 <Label htmlFor="author">Your Name</Label>
-                                <Control.text model=".author" name="author" id="author" placeholder="Your Name" className="form-control" />                                                            
+                                <Control.text model=".author" name="author" id="author" placeholder="Your Name" className="form-control" 
+                                    validators={{
+                                            required, 
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                /> 
+                                <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />                                                         
                             </div>
                             <div className="form-group">
                                 <Label htmlFor="text">Comment</Label>
