bd148a102a2486f4deb60e8186111c4ebacd672f
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-08-11 11:50:19 -0700

    Update MainComponent.js

--- NucampFolder/3-React/nucampsite/src/components/MainComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/MainComponent.js
@@ -65,7 +65,6 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/contactus' component={Contact} />
                     <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
                     <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                     <Redirect to='/home' />
