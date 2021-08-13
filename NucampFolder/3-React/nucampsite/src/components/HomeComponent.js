bde2a8d56acfabfe1a1807a501c754f6caa7065f
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-08-12 21:15:23 -0700

    ok

--- NucampFolder/3-React/nucampsite/src/components/HomeComponent.js
+++ NucampFolder/3-React/nucampsite/src/components/HomeComponent.js
@@ -40,10 +40,18 @@ export default function Home(props) {
                     />  
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    /> 
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                    <RenderCard
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    /> 
                 </div>
             </div>
         </div>
